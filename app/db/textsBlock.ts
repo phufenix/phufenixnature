98bcc735bc3f3b89cb646f2aea4048b5f3c43ddd
wieslawa <adrian.szymborski@cloudservices.pl>
2023-12-20 15:27:48 +0100

    ADDED TEKST

--- app/db/textsBlock.ts
+++ app/db/textsBlock.ts
@@ -2,7 +2,7 @@ const textBlocks = [
   {
     id: 'about_as',
     title: 'O nas',
-    text: `<p>Lorem ipsum</p>
+    text: `<p>Witaj w <span>Fenix Nature</span> - Twoim wyjątkowym źródle autentycznych i egzotycznych artykułów prosto z serca Madagaskaru! Jesteśmy dumni z tego, że możemy dostarczyć Ci unikalne produkty z tego magicznego kraju, znanego z niezwykłej różnorodności przyrody, bogatej kultury i tradycji.</p><br> <p>W naszym asortymencie znajdziesz szeroki wybór artykułów, począwszy od ręcznie wykonanych <span>ozdób</span> po egzotyczne <span>przyprawy</span>, <span>unikatowe tekstylia</span> i wiele innych. Dbamy o to, aby nasze produkty odzwierciedlały bogactwo kultury Madagaskaru, jednocześnie spełniając oczekiwania naszych klientów z całego świata.</p>
     `,
   },
 ];
